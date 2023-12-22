c82a4f366d9e3ee61d5710b335dd99604c8a272f
steven.mccoy <steven.mccoy@cira.ca>
2023-12-22 10:54:30 -0500

    Expose progress bar link click event. Move href check within this function  so it can be evaluated in realtime.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { useRouter, Link } from './src/lib/router-events';
+import { useRouter, Link, linkClicked } from './src/lib/router-events';
 import Next13NProgress from './src/Next13NProgress';
 
-export { Next13NProgress, useRouter, Link };
+export { Next13NProgress, useRouter, Link, linkClicked };
